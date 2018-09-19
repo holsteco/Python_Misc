89d9fbcc9860dba78b5a37fedffc2316f86ecdd9
holsteco <39134537+holsteco@users.noreply.github.com>
2018-09-19 06:52:12 -0400

    created

---
+++ random_number.py
@@ -0,0 +1,15 @@
+# corey b. holstege
+# 2018-09-19
+
+# import
+import random
+
+# take user input
+num_1 = int(input("Input the lower bound:"))
+num_2 = int(input("Input the upper bound:"))
+
+# main code
+my_int = random.randint(num_1, num_2)
+
+# print 
+print("Your random number is:", my_int)
